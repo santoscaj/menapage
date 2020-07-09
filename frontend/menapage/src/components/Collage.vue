59c58a796bf5cadb9541fea9390059b9f0f2e3d0
santoscaj <santosc.aj@gmail.com>
2020-07-08 21:48:47 -0400

    switched flickity for vue-carousel, more manageable

--- frontend/menapage/src/components/Collage.vue
+++ frontend/menapage/src/components/Collage.vue
@@ -1,40 +1,29 @@
 <template lang="pug">
 .page
-  .main-carousel
-    .carousel-cell(v-for="foto in fotos" :class="{'is-fullscreen': fullScreen }" )
+  Carousel.carousel(
+    :per-page="1"
+    :loop="true" 
+    :centerMode="true"
+    :navigationEnabled="true"
+    ) 
+    Slide.carousel-item( v-for="foto in fotos" :loop="true"  )
       img( :src="'data:image/jpeg;charset=utf-8;base64,' + albumFotos[foto.id]") 
-    //- .carousel-cell( :class="{'is-fullscreen': fullScreen }" )
-    //-   img( :src="'data:image/png;base64,' + img") 
-    //-   img( :src="'data:image/jpeg;charset=utf-8;base64,' + img") 
-    //- .carousel-cell( :class="{'is-fullscreen': fullScreen }" )
-    //-   img(src="https://eloutput.com/app/uploads-eloutput.com/2020/03/super-mario.jpg") 
-    //- .carousel-cell( :class="{'is-fullscreen': fullScreen }" )
-    //-   img(src="https://m.media-amazon.com/images/M/MV5BNDQ2YjE5OTctODY1NC00ZDgxLTk0YmUtYWExZGE1Njg0ZDRlXkEyXkFqcGdeQXVyNjMxNzQ2NTQ@._V1_UY1200_CR285,0,630,1200_AL_.jpg") 
-    //- .carousel-cell( :class="{'is-fullscreen': fullScreen }" )
-    //-   img(src="https://cdn.custom-cursor.com/collections/pokemon.png") 
-    //- .carousel-cell( :class="{'is-fullscreen': fullScreen }" )
-    //-   img(src="https://i.pinimg.com/474x/51/1a/04/511a0449d434c1d6f1e72155f15da67c.jpg") 
-    //- .carousel-cell( :class="{'is-fullscreen': fullScreen }" )
-    //-   img(src="https://cdn.vox-cdn.com/thumbor/Q6cr4ujTzXuv3NhRUIDkgZHxDdo=/0x0:2048x2048/920x613/filters:focal(861x861:1187x1187):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/65327601/mobile_MarioKartTour_screen_07_png_jpgcopy.0.jpg") 
-    //- .carousel-cell( :class="{'is-fullscreen': fullScreen }" )
-    //-   img(src="https://lh3.googleusercontent.com/jd_ZEZPrw1cb9jhh4ahq2AQmatyHooba15lDPyCTmne9Pffoy6yLaYxKpjcXGLPBi40") 
-    //- .carousel-cell( :class="{'is-fullscreen': fullScreen }" )
-    //-   img(src="https://www.videogameschronicle.com/files/2019/09/EEZSr_nUEAA8DUC-1024x1024.jpg") 
-  //- button.fullScreenBtn(@click="changeFullScreen()") fullscreen
-  //- .message-area
-  //-   button Message Berts
+      .caption
+        p testo berto
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue , Watch} from 'vue-property-decorator';
 import Flickity from 'flickity'
 import axios from 'axios'
+//@ts-ignore
+import { Carousel, Slide } from 'vue-carousel'
 import { Album, Foto, emptyAlbum, emptyFoto }  from '@/utils/Album'
 let backendUrl = 'http://localhost:3000'
 
-@Component({})
+@Component({components:{Carousel,Slide}})
 export default class Collage extends Vue {
-  fullScreen = true
+  fullScreen = false
   day         : number | null = null
   todaysAlbum : Album  = emptyAlbum()
   albumFotos  : Object = {} 
@@ -48,6 +37,13 @@ export default class Collage extends Vue {
     return this.todaysAlbum.fotos
   }
 
+  // get carouselFotos(){
+  //   if(this.fotos.length == 0) return []
+  //   let lastFoto = this.fotos[this.fotos.length]
+  //   let firstFoto = this.fotos[0]
+  //   return [lastFoto,...this.fotos, firstFoto]
+  // }
+
   async getFotos(fotos : Foto[]){
     for (let foto of fotos){
       try{
@@ -56,7 +52,6 @@ export default class Collage extends Vue {
         this.albumFotos = { ...this.albumFotos, [foto.id]: Buffer.from(response.data, 'binary').toString('base64') }
       }catch(err){console.error(err)}
     }
-    // this.updateCarousel()
   }
 
   @Watch('day')
@@ -67,33 +62,18 @@ export default class Collage extends Vue {
       let album : Album = response.data
       await this.getFotos(album.fotos)
       this.todaysAlbum = album
-      this.updateCarousel()
     }catch(err){console.error(err)}
   }
 
   timeChecker(){}
 
   created(){
-    this.day = new Date().getDate()
+    this.day = 12
+    // this.day = new Date().getDate()
     this.timeChecker()
   }
 
-  updateCarousel(){
-    let flkty   = new Flickity('.main-carousel', {
-      cellAlign: 'center', 
-      wrapAround: true,
-    })
-    document.querySelectorAll<HTMLElement>('.flickity-page-dots').forEach(element=>{
-      element.style.bottom= '10px'
-    })
-    document.querySelectorAll<HTMLElement>('.flickity-button').forEach( ( element )=>{
-      element.style.background  = 'rgb(0,0,0,0.1)'
-      element.style.color  = 'white'
-    })
-  }
-
   mounted(){
-    // this.updateCarousel()
   }
 }
 </script>
@@ -103,11 +83,6 @@ export default class Collage extends Vue {
 *
   box-sizing: border-box
 
-img
-  display: block
-  max-height: 100vh
-  max-width: 80vw
-
 .fullScreenBtn
   position: fixed
   top: 0
@@ -115,58 +90,61 @@ img
   background: red
   z-index: 1
 
-// .page
-//   display: flex 
-//   flex-direction: column
-//   height: 100%
-//   width: 100%
-//   justify-content: center
-//   align-items: center
+.page
+  border: 3px solid black
+  display: flex 
+  flex-direction: column
+  height: 100vh
+  width: 100vw
+  justify-content: center
+  align-items: center
 
 .image-area
   width: 100%
 
-.main-carousel
-  // height: 300px
+.carousel
+  overflow: hidden
+  // border: 1px solid red
+  display: flex
+  height: calc(100vh - 20px)
   // padding: 20px
   box-sizing: border-box
   // border: 1px solid green
-  // width: 100%
+  width: 100%
 
-.carousel-cell
-  background: #222
-  // width: 80%
-  width: 100vw
-  height: 100vh
-  counter-increment: carousel-cell
-  // for full-screen
+.carousel-item
   display: flex
-  align-items: center
+  background: white !important
   justify-content: center
-
-  // position: fixed
-  // left: 0
-  // top: 0
-  // width: 100vw
+  background: #222
   // height: 100vh
-  // background: hsla(0, 0%, 0%, 0.9)
-  // padding-bottom: 35px
-  // z-index: 1
+  max-width: 100%
+  max-height: 100%
+  &>img
+    display: block
+    // border: 3px solid white
+    max-height: calc(100vh - 20px)
+    max-width: calc(100vw - 20px)
+    width: auto
+    height: auto
+    object-fit: cover
 
-.is-fullscreen
-  position: fixed
-  left: 0
-  top: 0
-  width: 100vw
-  height: 100vh
-  background: hsla(0, 0%, 0%, 0.9)
-  // padding-bottom: 35px
-  z-index: 1
+// .is-fullscreen
+//   position: fixed
+//   left: 0
+//   top: 0
+//   width: 100vw
+//   height: 100vh
+//   background: hsla(0, 0%, 0%, 0.9)
+//   // padding-bottom: 35px
+//   z-index: 1
 
-.message-area
-  border: 1px solid pink
+.caption  
+  border: 1px solid red
   position: absolute
+  height: 200px
+  width: 100%
   bottom: 0
-  right: 0
+  
 
 </style>
