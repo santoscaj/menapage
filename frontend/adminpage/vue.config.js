05c66bdad3f8a893a3dc509153bfe30b220e1c4d
santoscaj <santosc.aj@gmail.com>
2020-07-16 10:57:24 -0400

    setting production build configuration

---
+++ frontend/adminpage/vue.config.js
@@ -0,0 +1,5 @@
+
+module.exports = {
+    // ...other vue-cli plugin options...
+    outputDir: '/var/www/adminmeninberto/html',
+}
--- frontend/menapage/vue.config.js
+++ frontend/menapage/vue.config.js
@@ -1,6 +1,7 @@
 
 module.exports = {
     // ...other vue-cli plugin options...
+    outputDir: '/var/www/meninberto/html',
     pwa: {
       themeColor: '#FFA400',
       msTileColor: '#000000',
